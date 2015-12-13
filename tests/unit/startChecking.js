0a7b077c9f006dc6288298212c40758f74a3efcf
Jenna Zeigen <jenna.zeigen@gmail.com>
2015-12-13 17:42:04 -0500

    test: add tests for stopChecking and startChecking

---
+++ tests/unit/startChecking.js
@@ -0,0 +1,63 @@
+var lolex = require('lolex')
+var simple = require('simple-mock')
+var test = require('tape')
+
+var startChecking = require('../../lib/startChecking')
+
+test('startChecking() with interval & 200 response', function (t) {
+  t.plan(2)
+
+  var clock = lolex.install(0)
+  var emitter = {emit: function () {}}
+
+  var state = {
+    method: 'HEAD',
+    url: 'https://example.com/ping',
+    emitter: emitter
+  }
+
+  simple.mock(startChecking.internals, 'check').callFn(function () {
+    return { then: function (success) { success() } }
+  })
+
+  startChecking(state, {
+    interval: 1000
+  })
+
+  t.ok(state.interval, 'state.interval is set')
+  clock.tick(2000)
+  t.is(startChecking.internals.check.callCount, 2, '2 requests sent')
+
+  clearTimeout(state.interval)
+  simple.restore()
+  clock.uninstall()
+})
+
+test('startChecking() with interval & 500 error response', function (t) {
+  t.plan(2)
+
+  var clock = lolex.install(0)
+  var emitter = {emit: function () {}}
+
+  var state = {
+    method: 'HEAD',
+    url: 'https://example.com/ping',
+    emitter: emitter
+  }
+
+  simple.mock(startChecking.internals, 'check').callFn(function () {
+    return { then: function (success, error) { error() } }
+  })
+
+  startChecking(state, {
+    interval: 1000
+  })
+
+  t.ok(state.interval, 'state.interval is set')
+  clock.tick(2000)
+  t.is(startChecking.internals.check.callCount, 2, '2 requests sent')
+
+  clearTimeout(state.interval)
+  simple.restore()
+  clock.uninstall()
+})
