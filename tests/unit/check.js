ea12b05866eef2ee2f090206bd74000e4b157e56
Gregor Martynus <gregor@martynus.net>
2016-01-16 16:53:14 +0100

    test(reset): abort pending requests
    * * *
    
    This commit was sponsored by Neighbourhoodie
    
    You can hire Neighbourhoodie for all your
    Hoodie / CouchDB / Offline First needs
    http://go.hood.ie/thehoodiefirm

--- tests/unit/check.js
+++ tests/unit/check.js
@@ -1,13 +1,14 @@
 var EventEmitter = require('events').EventEmitter
 
 var lolex = require('lolex')
+var Promise = require('lie')
 var simple = require('simple-mock')
 var test = require('tape')
 
 var check = require('../../lib/check')
 
 test('check() with 200 response', function (t) {
-  t.plan(5)
+  t.plan(4)
 
   // mocks
   var clock = lolex.install(0)
@@ -16,8 +17,15 @@ test('check() with 200 response', function (t) {
     t.is(state.url, 'https://example.com/ping', 'passes state to cache')
     t.is(arguments.length, 1, 'passes no error')
   })
-  simple.mock(check.internals, 'nets').callbackWith(null, {
-    statusCode: 200
+  simple.mock(check.internals, 'request').returnWith({
+    then: function (callback) {
+      callback({
+        statusCode: 200
+      })
+      return Promise.resolve()
+    },
+    catch: function () {},
+    abort: 'request abort method'
   })
 
   var state = {
@@ -28,11 +36,8 @@ test('check() with 200 response', function (t) {
   check(state)
 
     .then(function () {
-      t.ok(check.internals.nets.callCount, 1, 'request sent')
       t.is(state.timestamp, '1970-01-01T00:00:00.000Z', 'sets state.timestamp')
     })
-
-    .catch(t.fail)
 
     // cleanup mocks
     .then(function () {
@@ -42,7 +47,7 @@ test('check() with 200 response', function (t) {
 })
 
 test('check() with 500 response', function (t) {
-  t.plan(9)
+  t.plan(8)
 
   // mocks
   var clock = lolex.install(0)
@@ -51,8 +56,9 @@ test('check() with 500 response', function (t) {
     t.is(state.url, 'https://example.com/ping', 'passes state to cache')
     t.is(error.name, 'ServerError', 'passes error')
   })
-  simple.mock(check.internals, 'nets').callbackWith(null, {
-    statusCode: 500
+  simple.mock(check.internals, 'request').rejectWith({
+    name: 'ServerError',
+    code: 500
   })
 
   var emitter = new EventEmitter()
@@ -72,7 +78,6 @@ test('check() with 500 response', function (t) {
     })
 
     .catch(function (error) {
-      t.ok(check.internals.nets.callCount, 1, 'request sent')
       t.is(error.name, 'ServerError', 'error is ServerError')
       t.is(error.code, 500, 'error.code is 500')
       t.is(state.timestamp, '1970-01-01T00:00:00.000Z', 'sets state.timestamp')
@@ -87,7 +92,7 @@ test('check() with 500 response', function (t) {
 })
 
 test('check() with connection error', function (t) {
-  t.plan(6)
+  t.plan(5)
 
   // mocks
   var emitter = {emit: function () {}}
@@ -96,7 +101,9 @@ test('check() with connection error', function (t) {
     t.is(state.url, 'https://example.com/ping', 'passes state to cache')
     t.is(error.name, 'ConnectionError', 'passes error to cache')
   })
-  simple.mock(check.internals, 'nets').callbackWith(new Error('Oops'))
+  simple.mock(check.internals, 'request').rejectWith({
+    name: 'ConnectionError'
+  })
 
   check({
     method: 'HEAD',
@@ -111,7 +118,6 @@ test('check() with connection error', function (t) {
     .catch(function (error) {
       t.is(error.name, 'ConnectionError', 'error is ConnectionError')
       t.is(error.code, undefined, 'error.code is undefined')
-      t.ok(check.internals.nets.callCount, 1, 'request sent')
     })
 
     // cleanup mocks
@@ -125,7 +131,7 @@ test('check() with POST method', function (t) {
 
   // mocks
   simple.mock(check.internals.cache, 'set').callFn(function () {})
-  simple.mock(check.internals, 'nets').callbackWith(null, {})
+  simple.mock(check.internals, 'request').resolveWith({})
 
   check({
     method: 'POST',
@@ -133,10 +139,10 @@ test('check() with POST method', function (t) {
   })
 
     .then(function () {
-      t.is(check.internals.nets.lastCall.arg.method, 'POST', 'request sent with POST')
+      t.is(check.internals.request.lastCall.arg.method, 'POST', 'request sent with POST')
     })
 
-    .catch(t.fail)
+    .catch(t.error)
 
     // cleanup mocks
     .then(function () {
@@ -150,7 +156,9 @@ test('check() with timeout', function (t) {
   // mocks
   var emitter = {emit: function () {}}
   simple.mock(check.internals.cache, 'set').callFn(function () {})
-  simple.mock(check.internals, 'nets').callbackWith({code: 'ETIMEDOUT'})
+  simple.mock(check.internals, 'request').rejectWith({
+    name: 'TimeoutError'
+  })
 
   check({
     method: 'HEAD',
@@ -166,7 +174,7 @@ test('check() with timeout', function (t) {
     .catch(function (error) {
       t.is(error.name, 'TimeoutError', 'error is TimeoutError')
       t.is(error.code, undefined, 'error.code is undefined')
-      t.is(check.internals.nets.lastCall.arg.timeout, 1000, 'request sent with timeout')
+      t.is(check.internals.request.lastCall.arg.timeout, 1000, 'request sent with timeout')
     })
 
     // cleanup mocks
@@ -180,7 +188,7 @@ test('check() with 200 response when state.error is set', function (t) {
 
   // mocks
   simple.mock(check.internals.cache, 'set').callFn(function () {})
-  simple.mock(check.internals, 'nets').callbackWith(null, {})
+  simple.mock(check.internals, 'request').resolveWith({})
   var emitter = new EventEmitter()
   emitter.on('reconnect', function () {
     t.pass('"reconnect" event emitted')
@@ -196,13 +204,52 @@ test('check() with 200 response when state.error is set', function (t) {
   check(state)
 
     .then(function () {
-      t.is(check.internals.nets.callCount, 1, 'request sent')
+      t.is(check.internals.request.callCount, 1, 'request sent')
       t.is(state.error, undefined, 'removes state.error')
     })
 
-    .catch(t.fail)
+    .catch(t.error)
 
     // cleanup mocks
+    .then(function () {
+      simple.restore()
+    })
+})
+
+test('check() will abort existing request', function (t) {
+  t.plan(2)
+
+  // mocks
+  simple.mock(check.internals.cache, 'set').callFn(function () {})
+  simple.mock(check.internals, 'request').returnWith({
+    then: function (callback) {
+      callback({
+        statusCode: 200
+      })
+      return Promise.resolve()
+    },
+    catch: function () {},
+    abort: 'request abort method'
+  })
+
+  var abortStub = simple.stub()
+  var state = {
+    method: 'HEAD',
+    url: 'https://example.com/ping',
+    request: {
+      abort: abortStub
+    }
+  }
+
+  check(state)
+
+    .then(function () {
+      t.is(abortStub.callCount, 1)
+      t.is(state.request, undefined, 'removes request from state')
+    })
+
+    .catch(t.error)
+
     .then(function () {
       simple.restore()
     })
