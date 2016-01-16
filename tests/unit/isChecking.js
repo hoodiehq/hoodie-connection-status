f25135b297157f83fe2b612aafc6c4e805b79c54
Gregor Martynus <gregor@martynus.net>
2016-01-16 17:00:47 +0100

    test: remove unnecessary setTimeout
    * * *
    
    This commit was sponsored by Neighbourhoodie
    
    You can hire Neighbourhoodie for all your
    Hoodie / CouchDB / Offline First needs
    http://go.hood.ie/thehoodiefirm

--- tests/unit/isChecking.js
+++ tests/unit/isChecking.js
@@ -3,14 +3,10 @@ var test = require('tape')
 var isChecking = require('../../lib/isChecking')
 
 test('isChecking() when indeed checking', function (t) {
-  function check () {
-    return
-  }
-
   var state = {
     method: 'HEAD',
     url: 'https://example.com/ping',
-    interval: setTimeout(check, 5000, {}, {})
+    interval: 1
   }
 
   t.is(isChecking(state), true, 'is checking indeed')
