01b1b6eb6cb1b32fda74d042be47513fa9e26629
Nathan Stilwell <nathanstilwell@gmail.com>
2016-02-27 12:40:13 -0500

    test: reset(options)
    Making sure that reset treats options the same as parseOptions

--- tests/unit/reset.js
+++ tests/unit/reset.js
@@ -22,3 +22,22 @@ test('reset abort request', function (t) {
 
   t.is(state.request.abort.callCount, 1, 'request aborted')
 })
+
+test('reset interval to default', function (t) {
+  var intervalValue = 30000
+  var state = {
+    method: 'HEAD',
+    url: 'https://example.com/ping',
+    cache: {}
+  }
+  var options = {
+    interval: intervalValue
+  }
+
+  reset(state, options)
+
+  t.is(state.interval.connected, intervalValue, 'interval.connected reset')
+  t.is(state.interval.disconnected, intervalValue, 'interval.connected reset')
+
+  t.end()
+})
